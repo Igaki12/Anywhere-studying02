dc4121126d77fd63c18c96b18b4659656b363fb3
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2022-05-04 13:27:06 +0900

    renewed ControlPanel component

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,32 +1,46 @@
-import {Stack,Button,Spacer} from '@chakra-ui/react';
-import { ArrowBackIcon,CheckIcon } from '@chakra-ui/icons';
+import { Stack, Button, Spacer } from '@chakra-ui/react'
+import { ArrowBackIcon, CheckIcon, ChevronDownIcon } from '@chakra-ui/icons'
 export const ControlPanel = ({}) => {
   return (
     <Stack
-      bg={'teal'}
-      p="2"
-      bottom={'0'}
+      direction="column"
+      spacing={4}
+      align="center"
       position={'fixed'}
-      w="100%"
-      mt={'auto'}
-      direction="row"
+      bottom="0"
+      alignItems={'end'}
+      width="100%"
     >
-      <Button colorScheme="teal" variant="solid">
-        <ArrowBackIcon />
-      </Button>
-      <Spacer />
-      <Button
-        colorScheme="teal"
-        variant="solid"
-        minW={'200px'}
-        letterSpacing="0.1em"
-      >
-        Next
-      </Button>
-      <Spacer />
-      <Button colorScheme="teal" variant="solid">
-        <CheckIcon />
+      <Button colorScheme="teal" variant="outline">
+        <ChevronDownIcon />
       </Button>
     </Stack>
+
+    // <Stack
+    //   bg={'teal'}
+    //   p="2"
+    //   bottom={'0'}
+    //   position={'fixed'}
+    //   w="100%"
+    //   mt={'auto'}
+    //   direction="row"
+    // >
+    //   <Button colorScheme="teal" variant="solid">
+    //     <ArrowBackIcon />
+    //   </Button>
+    //   <Spacer />
+    //   <Button
+    //     colorScheme="teal"
+    //     variant="solid"
+    //     minW={'150px'}
+    //     letterSpacing="0.1em"
+    //   >
+    //     Next
+    //   </Button>
+    //   <Spacer />
+    //   <Button colorScheme="teal" variant="solid">
+    //     <CheckIcon />
+    //   </Button>
+    // </Stack>
   )
 }
