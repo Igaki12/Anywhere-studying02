07b589837836a4821fab03096ab060a73e25f94d
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2022-05-30 12:18:28 +0900

    更新履歴を改善

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -81,10 +81,16 @@ export const ControlPanel = ({ showSettingDetail, showHistory }) => {
               {history[history.length - 1].remainingQuestionList.length}問
             </Text>
             <Divider orientation="horizontal" mt={3} mb="1" />
-            <Text fontSize={'lg'}>更新履歴</Text>
-            <Text>2022-05-25 Ver0.7-”続きから再開”機能を仮実装</Text>
-            <Text>2022-05-22 Ver0.6-cookieを設定の引継ぎに利用</Text>
-            <Text>2022-05-09 Ver0.4-”キーワード絞り込み”機能を本格実装</Text>
+            <Text>更新履歴</Text>
+            <Text fontSize={'sm'}>
+              2022-05-25_Ver0.7-”続きから再開”機能を仮実装
+            </Text>
+            <Text fontSize={'sm'}>
+              2022-05-22_Ver0.6-cookieを設定の引継ぎに利用
+            </Text>
+            <Text fontSize={'sm'}>
+              2022-05-09_Ver0.4-”キーワード絞り込み”機能を本格実装
+            </Text>
           </ModalBody>
 
           <ModalFooter>
